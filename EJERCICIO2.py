df39a5ed472d7d908574f026546362b24ea4b6d3
paco.parente <pakoparente@gmail.com>
2025-05-20 16:04:23 -0300

    indica si un numeo es negativo, positivo o 0

---
+++ EJERCICIO2.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingresa un numero"))
+
+if numero > 0:
+    print(f"El numero, {numero} es positivo")
+elif numero < 0:
+    print (f"el numero, {numero} es negativo")
+ else 
+       print "el numero es cero"
