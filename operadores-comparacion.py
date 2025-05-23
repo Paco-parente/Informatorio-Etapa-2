69182ad52b67cfd4049680908a651e17232479a3
paco.parente <pakoparente@gmail.com>
2025-05-22 21:18:17 -0300

    operadores de comparacion

---
+++ operadores-comparacion.py
@@ -0,0 +1,39 @@
+a = 10
+b = 3
+
+
+reultado = a
+print(f"operador == : {a == b}")
+print(f"operador is : {a is b}")
+
+
+x = "hola"
+y = "hola"
+
+print(f"operador == : {x == y}")
+print(f"operador is : {x is y}")
+
+lista1 = [1, 2, 3,]
+lista2 = [1, 2, 3,]
+
+print(f"operador == : {lista1 == lista2}")
+print(f"operador is : {lista1 is lista2}")
+
+resultado = a != b
+print(f"operador !=: {resultado}")
+
+
+
+resultado = a < b
+print(f"operador <: {resultado}")
+
+ 
+
+resultado = a > b
+print(f"operador >: {resultado}")
+
+resultado = a >= b
+print(f"operador >=: {resultado}")
+
+resultado = a <= b
+print(f"operador <=: {resultado}")
