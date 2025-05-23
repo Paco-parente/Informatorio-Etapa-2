c8da37424c28547c2eacc76e0f4f5f490a5cc447
paco.parente <pakoparente@gmail.com>
2025-05-22 21:18:54 -0300

    operadores logicos

---
+++ operadores-logicos.py
@@ -0,0 +1,20 @@
+a = 10
+b = 3
+c = 20
+
+# AND
+
+resultado = a > b and a < c 
+
+print(f"operador and: {resultado}")
+
+
+#OR
+resultado = a > 20 or a < 5 
+print(f"operador or: {resultado}")
+
+
+#NOT
+
+resultado =  not a > b
+print(f"operador not: {resultado}")
