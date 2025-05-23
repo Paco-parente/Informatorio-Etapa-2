1ff830b99e6774e4d65518f7655c069e6b871cae
paco.parente <pakoparente@gmail.com>
2025-05-22 21:11:25 -0300

    operadores de pertenencia

---
+++ operadores-pertenencia.py
@@ -0,0 +1,26 @@
+a = 10
+b = 3
+
+texto = "hola mundo"
+
+lista = [1, 2, 3, 4, 5]
+
+# IN
+
+resultado = "mundo" in texto
+print(f"operador in: {resultado}")
+
+resultado = "holis" in texto
+print(f"operador in: {resultado}")
+
+
+resultado = 1 in lista
+print(f"operador in: {resultado}")
+
+resultado = 7 in lista
+print(f"operador in: {resultado}")
+
+resultado = 7 not in lista
+print(f"operador in: {resultado}")
+
+
