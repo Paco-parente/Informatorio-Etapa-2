10752740cd99f8bce1facf1dc8d4a025f6654648
paco.parente <pakoparente@gmail.com>
2025-05-29 12:35:11 -0300

    diccionarios su definicion y algunos usos

---
+++ colecciones_diccionarios.py
@@ -0,0 +1,19 @@
+ #se definen con llaves {} y estan compuestos por pares (clave-valor / key-value)
+#las claves son unicas, y los valores pueden ser cualquier tipo de datos, 
+# incluso otros diccionarios
+#son desordenados. (sus elementos no tienen indices) 
+# son mutables (podemos agregar, eliminar, modificar etc)
+
+estudiante = {
+    "nombre": "Ana",
+    "edad": 23,
+    "curso": "Matematicas",
+}
+
+print(f"Conjunto diccionario: {estudiante}, {type(estudiante)} ")
+
+print(f"Conjunto diccionario: {estudiante["edad"]}")
+
+estudiante["carrera"] = "ing"
+
+print(f"estudiante: {estudiante}")
