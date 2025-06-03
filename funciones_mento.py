62beafff1744feb76c9c36bb3cbeedd7e73e3b4d
paco.parente <pakoparente@gmail.com>
2025-06-03 19:42:28 -0300

    funcion de suma

---
+++ funciones_mento.py
@@ -0,0 +1,21 @@
+#definimos funciones ejemplos y usos
+
+
+# def sumar(a, b):
+#     return a + b
+
+# suma_de_ambos = sumar (20, 30)
+
+# print("la suma de estos numeros es", suma_de_ambos)
+
+
+def sumar(a, b):
+    return a + b
+
+print(f"la suma de estos numeros es, sumar(20, 25)")
+
+def sumar(a, b):
+    suma_ambos = a + b
+print(f"la suma de ambos es {sumar}") 
+
+
