2387c0135e5038c548b5e55fc2cea0cc56573515
paco.parente <pakoparente@gmail.com>
2025-06-03 19:40:49 -0300

    Primeras funciones

---
+++ funciones_mento_02.py
@@ -0,0 +1,16 @@
+
+
+
+# def multiplicar(a,b):
+#     return a * b
+
+# print(multiplicar(20,30))
+
+
+def presentacion_persona(nombre, edad, pais):
+   
+ print(f"Me llamo {nombre}, tengo {edad}, años y soy de {pais} " )
+
+presentacion_persona("argentina", "Andres", 34)
+   
+presentacion_persona(pais="argentina", nombre="Andres", edad=34)
